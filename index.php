f4a20406f1c7b244d3d66e33e5efa87a695dab55
aarsalannazari <aarsalannazari@gmail.com>
2023-10-29 15:21:29 +0330

    online-amooz

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload picture online-amooz</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+        }
+
+        h1,p,form{
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+    <h1>Online-amooz</h1>
+    <p>please choose a photo to send:</p>
+    <form action="#" method="post">
+        <input type="file" name="piture" id="">
+    </form>
+</body>
+</html>
